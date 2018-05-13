9435bbc3bf035752734befb2ad88a8634d9ee204
hmt <dev@hmt.im>
2018-05-14 00:34:55 +0200

    initial commit

---
+++ helfer.js
@@ -0,0 +1,63 @@
+import fkl from './daten/fachklassen.json'
+const noten = [null, 'sehr gut', 'gut', 'befriedigend', 'ausreichend', 'mangelhaft', 'ungenügend']
+const enoten = {'E1': 'mit besonderem Erfolg teilgenommen', 'E2': 'mit Erfolg teilgenommen', 'E3': 'teilgenommen'}
+const punkte = {
+  15: '1+', 14: '1', 13: '1-',
+  12: '2+', 11: '2', 10: '2-',
+   9: '3+',  8: '3',  7: '3-',
+   6: '4+',  5: '4',  4: '4-',
+   3: '5+',  2: '5',  1: '5-',
+             0: '6'}
+  const zahlwort = {1: "eins",2: "zwei",3: "drei",4: "vier",5: "fünf",6: "sechs",7: "sieben",8: "acht",9: "neun",0: "null"}
+
+export const datum = (t) => {
+  // gibt ein Datum im deutschen Format zurück
+  try {
+    return new Date(t).toLocaleDateString('de', {day: '2-digit', month: '2-digit', year: 'numeric'})
+  } catch (e) {console.log(e); return}
+}
+export const versetzungsvermerk = (s, hj, agz=null) => {
+  // gibt, wenn vorhanden den passenden Vermerk zurück
+  // wenn es ein agz ist und kein Konferenzdatum feststeht
+  if (agz && !hj.Konferenzdatum) return
+  let vermerk = ''
+  if (hj.Abschnitt === 2 || s.Klasse.startsWith('H')) {
+    switch (hj.VersetzungKrz) {
+      case 'N': vermerk = `Nicht versetzt laut Konferenzbeschluss vom ${datum(hj.Konferenzdatum)}`
+      case 'V': vermerk = `Versetzt laut Konferenzbeschluss vom ${datum(hj.Konferenzdatum)}`
+      default:  vermerk = 'Kein Versetzungsvermerk hinterlegt'
+    }
+  }
+  return vermerk
+}
+export const bemerkungen = (hj) => {
+  return hj.ZeugnisBem ? hj.ZeugnisBem.replace('\r\n', '<br/>') : 'keine'
+}
+export const volljaehrigBei = (s, datum) => {
+  // gibt an, ob der Schüler *s* zu einem Zeitpunkt *datum* volljährig war
+  try {
+  const g = new Date(s.Geburtsdatum)
+  const d = new Date(datum)
+  const volljaehrig = d.getFullYear() - g.getFullYear() - ((d.getMonth() > g.getMonth() || (d.getMonth() == g.getMonth() && d.getDay() >= g.getDay()) ? 0 : 1)) >= 18
+  return volljaehrig
+  } catch (e) {console.log(e); return}
+}
+export const schulform = (s) => {
+  switch (s.ASDSchulform[0]) {
+    case 'B': return 'Berufsfachschule'
+  }
+}
+export const bg = (s,k) => {
+  if (s.fachklasse && s.fachklasse.Kennung && fkl[s.fachklasse.Kennung]) {
+    return fkl[s.fachklasse.Kennung][k]
+  } else
+  return ''
+}
+export const note = (note) => noten[parseInt(note)] || enoten[note]
+export const punkte2note = (p) => punkte[parseInt(p)]
+export const noteInWorten = (n) => n.split('').map(n => n === ',' ? '/' : zahlwort[parseInt(n)]).join(' ')
+export const fremdsprache = (fach, fachklasse) => {
+  try {
+    return fach.Lernentw || fkl[fachklasse]['Fremdsprache'][fach.Fach_ID]
+  } catch (e) { return 'undefined - Sprachniveau fehlt'}
+}
---
+++ partials/SOA-Tabelle.html
@@ -0,0 +1,51 @@
+<table class="table table-bordered" style="width: 100%">
+  <tr>
+    <th width="35%">Bereichsspezifische Fächer</th>
+    <th>Zugeordnete Lernfelder</th>
+  </tr>
+  <tr>
+    <td>Erziehung und Soziales</td>
+    <td>
+      <ul class="dashes">
+        <li>Bedürfnisse und Potentiale wahrnehmen, beobachten und analysieren</li>
+        <li>Mit Konzepten in sozialpädagogischen Einrichtungen arbeiten</li>
+        <li>Beziehungen in der sozialpädagogischen und pflegerischen Arbeit gestalten</li>
+        <li>Gestaltungsprinzipien und Gestaltungsmittel in Räumen sozialpflegerischer Einrichtungen</li>
+      </ul>
+    </td>
+  </tr>
+  <tr>
+    <td>Gesundheitsförderung und Pflege</td>
+    <td>
+      <ul class="dashes">
+        <li>Sozialpflegerische Handlungsprozesse systematisch planen, umsetzen, dokumentieren und evaluieren</li>
+        <li>Pflegekonzepte als Grundlage der pflegerischen Tätigkeiten anwenden</li>
+        <li>Kompetenzen, Ressourcen und Potentiale erkennen und fördern</li>
+        <li>Gesunde Lebensführung fördern</li>
+      </ul>
+    </td>
+  </tr>
+  <tr>
+    <td>Arbeitsorganisation und Recht</td>
+    <td>
+      <ul class="dashes">
+        <li>Sich im Berufsfeld orientieren, auf Anforderungen einlassen und die eigene Person darstellen</li>
+        <li>Rechte und Pflichten des Sozialassistenten kennen und wahrnehmen</li>
+        <li>Außendarstellung der sozialpädagogischen bzw. -pflegerischen Einrichtungen unterstützen</li>
+        <li>Wirtschaftlich mit Materialien umgehen</li>
+      </ul>
+    </td>
+  </tr>
+</table>
+
+<style>
+  .table-bordered {
+    border-collapse: collapse;
+  }
+  .table-bordered tr {
+    border-bottom: thin solid #333;
+  }
+  .schulnummer {
+    text-align: right;
+  }
+</style>
