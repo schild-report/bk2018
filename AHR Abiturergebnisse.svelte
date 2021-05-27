f0d8d2c58ab1ee0dc34c4d4496d8789466432d00
hmt <dev@hmt.im>
2021-05-27 10:55:11 +0200

    Abiturergebnisse falsche Sortierung

--- AHR Abiturergebnisse.svelte
+++ AHR Abiturergebnisse.svelte
@@ -9,40 +9,35 @@
       <Voffset v=1/>
       <table class="table-bordered" width="100%">
         <tr>
-          <th rowspan="2" width="30px">Nr</th>
-          <th rowspan="2" style="text-align: left;">Name</th>
+          <th width="30px">Nr</th>
+          <th  style="text-align: left;">Name</th>
           <th colspan="2" width="50px">1. Fach (LK1)</th>
           <th colspan="2" width="50px">2. Fach (LK2)</th>
           <th colspan="2" width="50px">3. Fach (GK)</th>
           <th colspan="2" width="50px">4. Fach (mdl)</th>
-          <th rowspan="2" width="50px">Abitur- note</th>
-          <th rowspan="2" width="70px">Summe Block I</th>
-          <th rowspan="2" width="70px">vorl. Gesamt- summe</th>
-          <th rowspan="2" width="70px">Noten- sprung</th>
-          <th rowspan="2" width="50px">Diff</th>
-          <th rowspan="2" width="70px">mdl Pfl.- Prfg</th>
-          <th rowspan="2" width="70px">mdl Best.- Prfg</th>
-        </tr>
-        <tr>
-          <th>Fach</th><th>Note</th>
-          <th>Fach</th><th>Note</th>
-          <th>Fach</th><th>Note</th>
+          <th width="50px">Abitur- note</th>
+          <th width="70px">Summe Block I</th>
+          <th width="70px">vorl. Gesamt- summe</th>
+          <th width="70px">Noten- sprung</th>
+          <th width="50px">Diff</th>
+          <th width="70px">mdl Best.- Prfg</th>
+          <th width="70px">mdl Frw.- Prfg</th>
         </tr>
         {#each slice as s, ii}
           <tr>
             <td><b>{i*anzahl+ii+1}</b></td>
             <td style="text-align: left;"><b>{s.Name}, {s.Vorname}</b></td>
-            {#each s.abi_abschluss_faecher.filter(f => ['1', '2', '3', '4'].includes(f.AbiFach)).sort(f => f.AbiFach).reverse() as f}
+            {#each s.abi_abschluss_faecher.filter(f => ['1', '2', '3', '4'].includes(f.AbiFach)).sort(f => f.AbiFach) as f}
               <td>{f.FachKrz}</td>
-              <td>{f.AbiPruefErgebnis}</td>
+              <td>{f.AbiPruefErgebnis || "–"}</td>
             {/each}
-              <td>{s.abi_abschluss.Note}</td>
-              <td>{s.abi_abschluss.Punktsumme_I}</td>
-              <td>{s.abi_abschluss.GesamtPunktzahl}</td>
-              <td>{s.abi_abschluss.Notensprung}</td>
-              <td>{s.abi_abschluss.Notensprung && s.abi_abschluss.GesamtPunktzahl ? (s.abi_abschluss.Notensprung-s.abi_abschluss.GesamtPunktzahl) : ''}</td>
-              <td>{s.abi_abschluss_faecher.find(f=>f.MdlPflichtPruefung === '+') ? 'X' : ''}</td>
+              <td>{s.abi_abschluss.Note || "–"}</td>
+              <td>{s.abi_abschluss.Punktsumme_I || "–"}</td>
+              <td>{s.abi_abschluss.GesamtPunktzahl || "–"}</td>
+              <td>{s.abi_abschluss.Notensprung || "–"}</td>
+              <td>{s.abi_abschluss.Notensprung && s.abi_abschluss.GesamtPunktzahl ? (s.abi_abschluss.Notensprung-s.abi_abschluss.GesamtPunktzahl) : -''}</td>
               <td>{s.abi_abschluss_faecher.find(f=>f.MdlBestPruefung === '+') ? 'X' : ''}</td>
+              <td>{s.abi_abschluss_faecher.find(f=>f.MdlFreiwPruefung === '+') ? 'X' : ''}</td>
           </tr>
         {/each}
       </table>
