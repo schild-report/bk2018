ccb0b9d98de67400fdf70e2e6abda92dabadb60f
hmt <dev@hmt.im>
2022-02-09 13:38:30 +0100

    Zeige nichtbelegte Fächer

--- Allg Konferenzliste.svelte
+++ Allg Konferenzliste.svelte
@@ -1,12 +1,47 @@
+<script>
+  import { datum, chunk } from "./helfer";
+
+  import Voffset from "./partials/Voffset.svelte";
+
+  export let schueler, jahr, abschnitt, klasse;
+  const aktHalbjahr = (a) => a.Jahr === jahr && a.Abschnitt === abschnitt;
+  const heute = () =>
+    new Date().toLocaleDateString("de", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
+  const anzahl = 25;
+  const aktAbschnitt = () => schueler[0].abschnitte.slice(-1)[0];
+  const faecher = new Set();
+  schueler.forEach((s) =>
+    s.abschnitte.find(aktHalbjahr).noten.forEach((n) => {
+      faecher.add(n.fach.FachKrz);
+    })
+  );
+</script>
+
 {#each chunk(schueler, anzahl) as slice, i}
-  <div class="page grid" orientation="landscape" size="A3" style="font-size: 1.1rem">
+  <div
+    class="page grid"
+    orientation="landscape"
+    size="A3"
+    style="font-size: 1.0rem"
+  >
     <div class="header">
       <pre>
-        Konferenzliste <b>{klasse.Klasse || schueler[0].Klasse}</b> &Tab; {aktAbschnitt().klassenlehrer_in}: <b>{`${aktAbschnitt().lehrer?.Vorname} ${aktAbschnitt().lehrer?.Nachname} (${aktAbschnitt().lehrer?.Kuerzel})`}</b> &Tab; Stand: {heute()} &Tab; Konferenzdatum: {datum(aktAbschnitt().Konferenzdatum)} &Tab; Zeugnisdatum: {datum(aktAbschnitt().ZeugnisDatum)}
+        Konferenzliste <b>{klasse.Klasse || schueler[0].Klasse}</b
+        > &Tab; {aktAbschnitt().klassenlehrer_in}: <b
+          >{`${aktAbschnitt().lehrer?.Vorname} ${
+            aktAbschnitt().lehrer?.Nachname
+          } (${aktAbschnitt().lehrer?.Kuerzel})`}</b
+        > &Tab; Stand: {heute()} &Tab; Konferenzdatum: {datum(
+          aktAbschnitt().Konferenzdatum
+        )} &Tab; Zeugnisdatum: {datum(aktAbschnitt().ZeugnisDatum)}
       </pre>
     </div>
     <div class="main">
-      <Voffset v=1/>
+      <Voffset v="1" />
       <table>
         <tr>
           <th><b>Nr</b></th>
@@ -18,61 +53,62 @@
           {/each}
           <th><b>Bemerkungen</b></th>
         </tr>
-        <tr><td colspan="2" style="height: 4rem">Bitte abzeichnen</td>{#each Array.from(faecher) as f}<td></td>{/each}<td></td><td></td><td></td></tr>
+        <tr
+          ><td colspan="2" style="height: 4rem">Bitte abzeichnen</td
+          >{#each Array.from(faecher) as f}<td />{/each}<td /><td /><td /></tr
+        >
         {#each slice as s, ii}
           {#each s.abschnitte.filter(aktHalbjahr) as hj (hj.ID)}
             <tr>
-              <td><b>{i*anzahl+ii+1}</b></td>
+              <td><b>{i * anzahl + ii + 1}</b></td>
               <td><b>{s.Name}, {s.Vorname}</b></td>
               <td>{hj.SumFehlStd || 0}</td>
               <td>{hj.SumFehlStdU || 0}</td>
               {#each Array.from(faecher) as f}
-                {#each [hj.noten.find(n => n.fach.FachKrz === f)||{}] as n}
-                  <td class="text-center" style={parseInt(n.NotenKrz) > 4 ? 'background:tomato;':''}>
-                    <span class:kreis={n.Warnung==='+'}>{n.NotenKrz || '–'}</span>
-                  </td>
+                {#each [hj.noten.find((n) => n.fach.FachKrz === f)] as n}
+                  {#if n}
+                    <td
+                      class="text-center"
+                      style={parseInt(n.NotenKrz) > 4
+                        ? "background:tomato;"
+                        : ""}
+                    >
+                      <span class:kreis={n.Warnung === "+"}
+                        >{n.NotenKrz || "–"}</span
+                      >
+                    </td>
+                  {:else}<td style="background: lightgrey" />
+                  {/if}
                 {/each}
               {/each}
-              <td class="extraklein">{hj.ZeugnisBem || ''}</td>
+              <td class="extraklein">{hj.ZeugnisBem || ""}</td>
             </tr>
           {/each}
         {/each}
       </table>
     </div>
-    <div class="footer">Seite {i+1}/{Math.ceil(schueler.length/anzahl)}</div>
+    <div class="footer">
+      Seite {i + 1}/{Math.ceil(schueler.length / anzahl)}
+    </div>
   </div>
 {/each}
 
-<script>
-  import { datum, chunk }  from './helfer'
-
-  import Voffset from './partials/Voffset.svelte'
-
-  export let schueler, jahr, abschnitt, klasse
-  const aktHalbjahr = (a) => a.Jahr === jahr && a.Abschnitt === abschnitt
-  const heute = () => new Date().toLocaleDateString('de', {day: '2-digit', month: '2-digit', year: 'numeric'})
-  const anzahl = 25
-  const aktAbschnitt = () => schueler[0].abschnitte.slice(-1)[0]
-  const faecher = new Set
-  schueler.forEach(s => s.abschnitte.find(aktHalbjahr).noten.forEach(n => {faecher.add(n.fach.FachKrz)}))
-</script>
-
 <style>
-  @import 'css/main.css';
+  @import "css/main.css";
   table {
     border-collapse: collapse;
-    width:auto;
+    width: auto;
   }
   table td {
     border: thin solid #ddd;
     line-height: 1.3rem;
     padding: 9px;
-    white-space:nowrap;
+    white-space: nowrap;
   }
   table td.extraklein {
-    font-size: .6rem;
-    white-space:pre-wrap;
-    word-wrap:break-word;
+    font-size: 0.6rem;
+    white-space: pre-wrap;
+    word-wrap: break-word;
     vertical-align: top;
     padding: 1px;
   }
@@ -84,22 +120,18 @@
     white-space: nowrap;
   }
   th.rotate > div {
-    transform:
-      translate(20px, -3px)
-      rotate(315deg);
+    transform: translate(20px, -3px) rotate(315deg);
     box-sizing: border-box;
   }
   th.rotate > div > span {
     border-bottom: 1px solid #ccc;
     width: 30px;
     transform-origin: left bottom;
-    display: inline-block
+    display: inline-block;
   }
   .kreis {
     border: 0.2rem solid red;
     border-radius: 20%;
     padding: 0.2rem;
   }
-
 </style>
-
