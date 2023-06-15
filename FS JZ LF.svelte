cfe5fb8e3b27abe5534dcb910a5d243307051130
hmt <dev@hmt.im>
2023-06-15 14:17:22 +0200

    fs jz

--- FS JZ LF.svelte
+++ FS JZ LF.svelte
@@ -1,6 +1,6 @@
 {#each schueler as s (s.ID)}
   {#each s.abschnitte.filter(aktHalbjahr) as hj (hj.ID)}
-    <div class="page grid" orientation="portrait" size="A4" style="font-size: .85rem">
+    <div class="page grid" orientation="portrait" size="A4" style="font-size: .9rem">
       <Pageheader art="daten/zeugnis.svg" logo="{privat.logo}" untertitel="{privat.untertitel}" traeger="{privat.traeger}"/>
       <div class="main">
         <div style="font-size: 13.5pt; line-height: 1.5em">
@@ -19,7 +19,7 @@
         {/if}
         <Voffset v="2"/>
         Leistungen laut Konferenzbeschluss vom {datum(hj.Konferenzdatum)}:
-        <div style="font-size: 0.9rem;">
+        <div>
           <Noten lernfeld noten={hj.noten} fachGliederungen={s.fachklasse.fach_gliederungen}></Noten>
         </div>
         <hr />
